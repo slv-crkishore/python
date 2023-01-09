8b4c6bb8ea7133a408874b264a1b5a442f1d8a4b
kishore <ramkishore.ram3@gmail.com>
2023-01-10 04:51:24 +0530

    How do you merge two dictionaries in Python?

---
+++ prog_24.py
@@ -0,0 +1,8 @@
+'''How do you merge two dictionaries in Python?'''
+
+dict1 = {'name': 'ram', 'age': 30}
+dict2 = {'city': 'manhattan', 'country': 'pakisthan', 'name': "ramesh"}
+
+dict1.update(dict2)
+
+print(dict1)
