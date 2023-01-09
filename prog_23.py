78a3a63f8eb1f9523b459dc36d37932cd6db8501
kishore <ramkishore.ram3@gmail.com>
2023-01-10 04:46:14 +0530

    How do you check if a key exists in a dictionary in Python?

---
+++ prog_23.py
@@ -0,0 +1,14 @@
+'''How do you check if a key exists in a dictionary in Python?'''
+
+
+def diction():
+    my_list = {
+        "name": "borderland",
+        "age": "34"
+    }
+
+    if 'age' in my_list:
+        print("key exists in dictionary")
+
+
+diction()
