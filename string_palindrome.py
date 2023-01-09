12cb553ce92c6843274143323c40601b2005ca45
kishore <ramkishore.ram3@gmail.com>
2023-01-09 18:51:49 +0530

    find the given strings palindrome or not

---
+++ string_palindrome.py
@@ -0,0 +1,28 @@
+'''find all the palindromes in the given strings'''
+
+
+# def palindrome(list_items):
+
+#     # for items in list_items:
+#     if list_items == list_items[::-1]:
+#         print(list_items + " "+"is a palindrome")
+
+#     else:
+#         print(list_items + " " + "is" + " not a palindrome")
+
+
+# palindrome(list_items=input("enter the strings:"))
+
+
+def pal(list_it):
+    temp = ""
+    # temp1 = ""
+    for items in list_it:
+        temp += items
+    if temp == list_it:
+        print(temp + " is a palindrome")
+    else:
+        print(list_it + " is not a palindrome")
+
+
+pal(list_it=input("enter the strings:"))
