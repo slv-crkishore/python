92223ee4e30f25805d0ac2ce694aeef13ae2f6e9
kishore <ramkishore.ram3@gmail.com>
2023-01-10 04:53:51 +0530

    path_exercise

---
+++ path_1.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+base_path = Path(r"D:\my_prog\RuleCreation.csv")
+m = Path(r"D:\my_prog")
+print(base_path.stem)
+# print(list(base_path.iterdir()))
+print(base_path.parent.parent)
+# breakpoint()
+
+for i in base_path.parent.iterdir():
+    print(i.name)
