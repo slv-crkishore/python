8ec458dee234371b73f561cbf7f93e32b51539bc
kishore <ramkishore.ram3@gmail.com>
2023-01-10 12:29:34 +0530

    rite a function that takes in a string and returns a new string with all the letters in alphabetical order.

---
+++ prog_26.py
@@ -0,0 +1,18 @@
+'''Write a function that takes in a string and returns a new string with all the letters in alphabetical order.'''
+
+
+def alphabetic_order(string):
+    temp = [letter for letter in string]
+    temp1 = []
+    while temp:
+        min = temp[0]
+        for i in temp:
+            if i < min:
+                min = i
+        temp1.append(min)
+        temp.remove(min)
+
+    return "".join(temp1)
+
+
+print(alphabetic_order(string='kishore'))
